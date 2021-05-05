2277c06e7031e12b23c2e550c90e49e0e904aab7
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-05-05 12:40:37 -0700

    add message

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const { App } = require("@slack/bolt");
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token:
-    "xoxp-2020354734098-2005581802407-2040523958260-5b982ff1eb3f5644acd62e9b0b71141b",
-  signingSecret: "xoxb-2020354734098-2031285094117-U3bzZCDpqTtsB1T1XgseJAsq",
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// Listens to incoming messages that contain "goodbye"
+app.message("goodbye", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`See ya later, <@${message.user}> :wave:`);
 });
 
 (async () => {
